8568a4cc9155cfd244af928c7b24d6e0baf1a9bf
ageu_dev19 <ageumenezes423@gmail.com>
2024-11-17 19:08:39 -0300

    Creates additional Middleware

--- routes/web.php
+++ routes/web.php
@@ -3,11 +3,13 @@
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\MainController;
 use App\Http\Middleware\CheckIsLoggedIn;
-// use App\Http\Controllers\MainController;
+use App\Http\Middleware\CheckIsNotLoggedIn;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/login', [AuthController::class, 'login']);
-Route::post('/loginSubmit', [AuthController::class, 'loginSubmit']);
+Route::middleware([CheckIsNotLoggedIn::class])->group(function() {
+  Route::get('/login', [AuthController::class, 'login']);
+  Route::post('/loginSubmit', [AuthController::class, 'loginSubmit']);
+});
 
 Route::middleware([CheckIsLoggedIn::class])->group(function() {
   Route::get('/', [MainController::class, 'index']);
